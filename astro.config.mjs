d72fc29261e412ac3b7d02e5c41ab2804092ff8b
Makuma <yube.hitomakumar@email.com>
2026-08-03 12:38:39 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://makuma1005.com',
+  integrations: [sitemap()]
+});
